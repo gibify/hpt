318000ea5b87a312f1e022abc467a6275c1b5573
gibify <evandrogibicoski@outlook.com>
2020-10-03 20:31:50 -0300

    fix rules page

--- src/pages/Rules/styles.ts
+++ src/pages/Rules/styles.ts
@@ -11,7 +11,7 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
+    justify-content: space-between;
 `;
 
 export const Content = styled.div`
